7bae5fe770fc1cdd674c7d90124df340dd8c4ab9
Olivier TERRIEN <oterrien@neuf.fr>
2016-06-07 00:26:49 +0200

    Create TestJUnitRule.java

---
+++ src/test/java/com/ote/test/TestJUnitRule.java
@@ -0,0 +1,69 @@
+package com.ote.test;
+
+import lombok.NonNull;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestName;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+public class TestJUnitRule {
+
+    @Rule
+    public TestRule testRule = RuleChain.outerRule(new LoggingRule("Message")).around(new MyRule());
+
+    @Rule
+    public TestName testNameRule = new TestName();
+
+    private void testParameter(@NonNull String test){
+        System.out.println(test);
+    }
+
+    @Test
+    public void test() {
+        System.out.println("INNER TEST : " + testNameRule.getMethodName());
+
+        testParameter(null);
+    }
+
+    public class LoggingRule implements TestRule {
+
+        private String message;
+
+        public LoggingRule(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public Statement apply(Statement statement, Description description) {
+
+            return new Statement() {
+
+                @Override
+                public void evaluate() throws Throwable {
+                    System.out.println(message);
+                    statement.evaluate();
+                }
+            };
+        }
+    }
+
+    public static class MyRule implements TestRule {
+
+        @Override
+        public Statement apply(Statement statement, Description description) {
+
+            return new Statement() {
+
+                @Override
+                public void evaluate() throws Throwable {
+                    System.out.println("BEFORE : " + description);
+                    statement.evaluate();
+                    System.out.println("AFTER : " + description);
+                }
+            };
+        }
+    }
+}
